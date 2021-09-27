ed6cd65ef342fbefbd1c13079b22340d6848ddc3
anirban-baisya <anirban.baisya2020@vitbhopal.ac.in>
2021-09-27 10:17:35 +0530

    First Commit

--- src/app.js
+++ src/app.js
@@ -26,12 +26,20 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for (let i = 0; i< players.length; i++) {
+        let character = {};
+        character.name = players[i];
+        character.strength = 100;
+        character.image = `images/super-${i + 1}.png`;
+        character.type = "hero";
+        detailedPlayers.push(character);
+    }
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
+    return Math.ceil(Math.random() *100)+1; //for getting the value 0 to 100;
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
 }
@@ -42,6 +50,17 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+
+    for (var i = 0; i < players.length; i++) { //creating multipe players
+        fragment =
+
+            `<div class="player">
+            <img src="${players[i].image}" alt="">
+        <div class="name">${players[i].name}</div>
+        <div class="strength">${players[i].strength}</div>
+    </div>`;
+
+    }
 
     return fragment;
 }
